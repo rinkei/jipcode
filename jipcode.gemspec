11cfc256e63c8d6632a4ccc11018964158e99bbd
Kei Hayashi <kei.h.hayashi@gmail.com>
2018-03-21 16:07:31 +0900

    ダウンロードした郵便番号データから郵便番号ごとに住所をファイルに保存する
    * 全郵便番号データをZIPから展開するためにライブラリのrubyzipを使用
    * 全郵便番号データの文字コードをSHIFT JISからUTF-8に変換するため標準ライブラリのnkfを使用
    * 全郵便番号データのCSVをパースするためにライブラリ標準ライブラリのcsvを使用
    * 保存する情報は都道府県、市区町村、町域のCSV

--- jipcode.gemspec
+++ jipcode.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'rubyzip'
 end
--- lib/jipcode/japan_post.rb
+++ lib/jipcode/japan_post.rb
@@ -1,18 +1,79 @@
+require 'jipcode'
 require 'uri'
 require 'net/http'
+require 'zip'
+require 'nkf'
 
 module Jipcode
   module JapanPost
-    ZIPCODE_URL = 'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip'.freeze
+    ZIPCODE_URLS = {
+      general: 'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip'.freeze
+    }.freeze
+    ZIPCODE_FILES = {
+      general: 'KEN_ALL.CSV'.freeze
+    }.freeze
 
-    def download
-      url = URI.parse(ZIPCODE_URL)
-      Net::HTTP.start(url.host, url.port) do |http|
-        res = http.get(url.path)
-        File.open('ken_all.zip', 'wb') { |f| f.write(res.body) }
+    def download_all
+      ZIPCODE_URLS.each do |type, url|
+        url = URI.parse(url)
+        Net::HTTP.start(url.host, url.port) do |http|
+          res = http.get(url.path)
+          File.open("zipcode/#{type}.zip", 'wb') { |f| f.write(res.body) }
+        end
       end
     end
 
-    module_function :download
+    def import_all
+      File.rename(ZIPCODE_PATH, 'zipcode/previous') if File.exist?(ZIPCODE_PATH)
+      Dir.mkdir(ZIPCODE_PATH)
+
+      zipcodes = unpack(:general)
+      import(zipcodes) do |row|
+        zipcode    = row[2] # 郵便番号
+        prefecture = row[6] # 都道府県
+        city       = row[7] # 市区町村
+        # 郵便番号の下2桁が00の時、町域指定されない。
+        # その場合、町域は「以下に掲載がない場合」となっているのでnilにする。
+        town       = zipcode[5..6] == '00' ? nil : row[8]
+
+        [zipcode, prefecture, city, town]
+      end
+
+      FileUtils.rm_rf('zipcode/previous')
+    rescue => e
+      FileUtils.rm_rf(ZIPCODE_PATH)
+      File.rename('zipcode/previous', ZIPCODE_PATH) if File.exist?('zipcode/previous')
+      raise e, '日本郵便のデータを読み込めませんでした。'
+    end
+
+    # Private
+
+    def unpack(type)
+      download unless File.exist?("zipcode/#{type}.zip")
+
+      content = ::Zip::File.open("zipcode/#{type}.zip") do |zip_file|
+                  entry = zip_file.glob(ZIPCODE_FILES[type]).first
+                  raise '日本郵便のファイルからデータが見つかりませんでした。' unless entry
+                  entry.get_input_stream.read
+                end
+
+      # 文字コードをSHIFT JISからUTF-8に変換
+      NKF.nkf('-w -Lu', content)
+    end
+
+    def import(zipcodes)
+      CSV.parse zipcodes do |row|
+        address = yield(row)
+        zipcode = address.shift
+
+        # 郵便番号は10万件以上あるので上3桁ごとにディレクトリを分割
+        dir = "#{ZIPCODE_PATH}/#{zipcode[0..2]}"
+        Dir.mkdir(dir) unless Dir.exist?(dir)
+        open("#{dir}/#{zipcode[3..6]}.csv", 'a') { |f| f.write("#{address.join(',')}\n") }
+      end
+    end
+
+    module_function :download_all, :import_all, :unpack, :import
+    private_class_method :unpack, :import
   end
 end
