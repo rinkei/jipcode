c80218aac0e981128bd82fad3ca41c27ff0800c7
Kei Hayashi <kei.h.hayashi@gmail.com>
2018-03-21 16:25:36 +0900

    郵便番号から対応する住所を検索する機能を追加

--- lib/jipcode.rb
+++ lib/jipcode.rb
@@ -1,5 +1,15 @@
 require "jipcode/version"
+require 'csv'
 
 module Jipcode
-  # Your code goes here...
+  ZIPCODE_PATH = 'zipcode/latest'.freeze
+
+  def search(zipcode)
+    path = "#{ZIPCODE_PATH}/#{zipcode[0..2]}/#{zipcode[3..6]}.csv"
+    return [] unless File.exist?(path)
+    addresses = open(path) { |f| f.read }
+    CSV.parse(addresses)
+  end
+
+  module_function :search
 end
--- spec/jipcode_spec.rb
+++ spec/jipcode_spec.rb
@@ -3,7 +3,34 @@ RSpec.describe Jipcode do
     expect(Jipcode::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.search' do
+    before(:all) do
+      Dir.mkdir("#{Jipcode::ZIPCODE_PATH}/xxx")
+      open("#{Jipcode::ZIPCODE_PATH}/xxx/0001.csv", 'w') do |f|
+        f.write("HOGE県,hoge市,ほげ\nFUGA県,fuga市,ふが")
+      end
+    end
+
+    after(:all) do
+      FileUtils.rm_rf("#{Jipcode::ZIPCODE_PATH}/xxx")
+    end
+
+    subject { Jipcode.search(zipcode) }
+
+    context '引数の郵便番号に対応する住所がない時' do
+      let(:zipcode) { 'xxx0000' }
+
+      it '空配列を返す' do
+        is_expected.to eq []
+      end
+    end
+
+    context '引数の郵便番号に対応する住所がある時' do
+      let(:zipcode) { 'xxx0001' }
+
+      it '対応する住所を全て含む配列を返す' do
+        is_expected.to eq [['HOGE県', 'hoge市', 'ほげ'], ['FUGA県', 'fuga市', 'ふが']]
+      end
+    end
   end
 end
