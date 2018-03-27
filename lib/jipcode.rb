f8d11f637cc1092cca1334501af2639b8273b98e
Kei Hayashi <kei.h.hayashi@gmail.com>
2018-03-27 21:40:59 +0900

    検索結果の出力が配列だと使いにくいのでHashを返す.locateを追加
    .searchはmixinすると被りやすいのでDEPRECATEDにする

--- lib/jipcode.rb
+++ lib/jipcode.rb
@@ -4,12 +4,29 @@ require 'csv'
 module Jipcode
   ZIPCODE_PATH = "#{File.dirname(__FILE__)}/../zipcode/latest".freeze
 
+  def locate(zipcode)
+    path = "#{ZIPCODE_PATH}/#{zipcode[0..2]}.csv"
+    return [] unless File.exist?(path)
+
+    addresses_csv = open(path) { |f| f.read }
+    addresses_array = CSV.parse(addresses_csv).select { |address| address[0] == zipcode }
+    addresses_array.map do |address_param|
+      {
+        zipcode:    address_param[0],
+        prefecture: address_param[1],
+        city:       address_param[2],
+        town:       address_param[3]
+      }
+    end
+  end
+
   def search(zipcode)
+    warn '[DEPRECATION] `search` is deprecated.  Please use `locate` instead.'
     path = "#{ZIPCODE_PATH}/#{zipcode[0..2]}.csv"
     return [] unless File.exist?(path)
     addresses = open(path) { |f| f.read }
     CSV.parse(addresses).select { |address| address[0] == zipcode }
   end
 
-  module_function :search
+  module_function :locate, :search
 end
--- spec/jipcode_spec.rb
+++ spec/jipcode_spec.rb
@@ -3,6 +3,39 @@ RSpec.describe Jipcode do
     expect(Jipcode::VERSION).not_to be nil
   end
 
+  describe '.locate' do
+    before(:all) do
+      open("#{Jipcode::ZIPCODE_PATH}/xxx.csv", 'w') do |f|
+        f.write("xxx0001,HOGE県,hoge市,ほげ\nxxx0001,FUGA県,fuga市,ふが\nxxx0002,PIYO県,piyo市,ぴよ")
+      end
+    end
+
+    after(:all) do
+      File.delete("#{Jipcode::ZIPCODE_PATH}/xxx.csv")
+    end
+
+    subject { Jipcode.locate(zipcode) }
+
+    context '引数の郵便番号に対応する住所がない時' do
+      let(:zipcode) { 'xxx0000' }
+
+      it '空配列を返す' do
+        is_expected.to eq []
+      end
+    end
+
+    context '引数の郵便番号に対応する住所がある時' do
+      let(:zipcode) { 'xxx0001' }
+
+      it '対応する住所を全て含む配列を返す' do
+        is_expected.to eq [
+          { zipcode: 'xxx0001', prefecture: 'HOGE県', city: 'hoge市', town: 'ほげ' },
+          { zipcode: 'xxx0001', prefecture: 'FUGA県', city: 'fuga市', town: 'ふが' }
+        ]
+      end
+    end
+  end
+
   describe '.search' do
     before(:all) do
       open("#{Jipcode::ZIPCODE_PATH}/xxx.csv", 'w') do |f|
