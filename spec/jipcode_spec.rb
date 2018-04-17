b61340717c88c6739bbff0e92b17778d2625dc0b
Kei Hayashi <kei.h.hayashi@gmail.com>
2018-04-18 04:44:22 +0900

    .locateの入力に制限をかけたことに対するテストの修正

--- spec/jipcode_spec.rb
+++ spec/jipcode_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe Jipcode do
 
   describe '.locate' do
     before(:all) do
-      open("#{Jipcode::ZIPCODE_PATH}/xxx.csv", 'w') do |f|
-        f.write("xxx0001,HOGE県,hoge市,ほげ\nxxx0001,FUGA県,fuga市,ふが\nxxx0002,PIYO県,piyo市,ぴよ")
+      open("#{Jipcode::ZIPCODE_PATH}/000.csv", 'w') do |f|
+        f.write("0000001,HOGE県,hoge市,ほげ\n0000001,FUGA県,fuga市,ふが\n0000002,PIYO県,piyo市,ぴよ")
       end
     end
 
     after(:all) do
-      File.delete("#{Jipcode::ZIPCODE_PATH}/xxx.csv")
+      File.delete("#{Jipcode::ZIPCODE_PATH}/000.csv")
     end
 
     subject { Jipcode.locate(zipcode) }
 
     context '引数の郵便番号に対応する住所がない時' do
-      let(:zipcode) { 'xxx0000' }
+      let(:zipcode) { '0000000' }
 
       it '空配列を返す' do
         is_expected.to eq []
@@ -25,12 +25,12 @@ RSpec.describe Jipcode do
     end
 
     context '引数の郵便番号に対応する住所がある時' do
-      let(:zipcode) { 'xxx0001' }
+      let(:zipcode) { '0000001' }
 
       it '対応する住所を全て含む配列を返す' do
         is_expected.to eq [
-          { zipcode: 'xxx0001', prefecture: 'HOGE県', city: 'hoge市', town: 'ほげ' },
-          { zipcode: 'xxx0001', prefecture: 'FUGA県', city: 'fuga市', town: 'ふが' }
+          { zipcode: '0000001', prefecture: 'HOGE県', city: 'hoge市', town: 'ほげ' },
+          { zipcode: '0000001', prefecture: 'FUGA県', city: 'fuga市', town: 'ふが' }
         ]
       end
     end
