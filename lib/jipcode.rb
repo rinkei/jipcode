615f671c95ac6f342cb6cde82edde7619cd65555
pione30 <g.pionelish30@gmail.com>
2019-05-15 17:54:24 +0900

    add prefecture_code into addresses_array

--- lib/jipcode.rb
+++ lib/jipcode.rb
@@ -1,8 +1,10 @@
 require "jipcode/version"
 require 'csv'
+require 'yaml'
 
 module Jipcode
   ZIPCODE_PATH = "#{File.dirname(__FILE__)}/../zipcode/latest".freeze
+  PREFECTURE_CODE = YAML.load_file("#{File.dirname(__FILE__)}/../data/prefecture_code.yml").freeze
 
   def locate(zipcode)
     # 数字7桁以外の入力は受け付けない
@@ -15,10 +17,11 @@ module Jipcode
     addresses_array = CSV.read(path).select { |address| address[0] == zipcode }
     addresses_array.map do |address_param|
       {
-        zipcode:    address_param[0],
-        prefecture: address_param[1],
-        city:       address_param[2],
-        town:       address_param[3]
+        zipcode:         address_param[0],
+        prefecture:      address_param[1],
+        city:            address_param[2],
+        town:            address_param[3],
+        prefecture_code: PREFECTURE_CODE.invert[address_param[1]]
       }
     end
   end
