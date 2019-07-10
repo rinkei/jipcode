e227f35d231bd1481669ed29bd08ca0ea57acc9f
pione30 <g.pionelish30@gmail.com>
2019-07-11 00:17:46 +0900

    it should be optional to extend elements out of KEN_ALL.CSV

--- lib/jipcode.rb
+++ lib/jipcode.rb
@@ -6,7 +6,7 @@ module Jipcode
   ZIPCODE_PATH = "#{File.dirname(__FILE__)}/../zipcode/latest".freeze
   PREFECTURE_CODE = YAML.load_file("#{File.dirname(__FILE__)}/../data/prefecture_code.yml").freeze
 
-  def locate(zipcode)
+  def locate(zipcode, opt={})
     # 数字7桁以外の入力は受け付けない
     return [] unless zipcode =~ /\A\d{7}?\z/
 
@@ -15,16 +15,28 @@ module Jipcode
     return [] unless File.exist?(path)
 
     addresses_array = CSV.read(path).select { |address| address[0] == zipcode }
-    addresses_array.map do |address_param|
-      {
-        zipcode:         address_param[0],
-        prefecture:      address_param[1],
-        city:            address_param[2],
-        town:            address_param[3],
-        prefecture_code: PREFECTURE_CODE.invert[address_param[1]]
-      }
+
+    if opt.empty?
+      addresses_array.map { |address_param| basic_address_from(address_param) }
+    else
+      addresses_array.map { |address_param| extended_address_from(address_param, opt) }
     end
   end
 
-  module_function :locate
+  def basic_address_from(address_param)
+    {
+      zipcode:    address_param[0],
+      prefecture: address_param[1],
+      city:       address_param[2],
+      town:       address_param[3]
+    }
+  end
+
+  def extended_address_from(address_param, opt={})
+    address = basic_address_from(address_param)
+    address[:prefecture_code] = PREFECTURE_CODE.invert[address_param[1]] if opt[:prefecture_code]
+    address
+  end
+
+  module_function :locate, :basic_address_from, :extended_address_from
 end
